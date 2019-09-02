6af79878345906606a908e91f24b434828e528cc
Võ Huỳnh Minh Tân <vohuynhminhtan@gmail.com>
2019-09-02 21:39:19 +0700

    finishing sign in hanhleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ class App extends React.Component {
             currentUser: snapShot.id,
             ...snapShot.data()
           });
-          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
